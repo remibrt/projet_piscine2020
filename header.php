703910c77d8c9e136b2a9598358ff0daf4195889
Breton Remi <remib37@gmail.com>
2020-04-17 20:06:20 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ebay ECE - Accueil</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+ 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-md">
+
+		<div class="nav-item dropdown">
+      		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"aria-expanded="false">
+       			Menu
+      		</a>
+      		<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+       			<a class="dropdown-item" href="categories.php"><p>Catégories</p></a>
+        		<a class="dropdown-item" href="acheter.php"><p>Acheter</p></a>
+        		<a class="dropdown-item" href="vendre.php"><p>Vendre</p></a>
+        	</div>
+        </div>
+
+		<a class="navbar-brand" href="index.php">Ebay ECE</a>
+		<img src="img/marteau.png" style="height: 30px;">
+		<h1 class="navbar-brand" style="margin-left: 300px;">Enchères en Ligne</h1>
+		<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+ 			<span class="navbar-toggler-icon"></span>
+ 		</button>
+
+ 		<div class="collapse navbar-collapse" id="main-navigation">
+ 		<ul class="navbar-nav">
+ 			<li class="nav-item"><a class="nav-link" href="Votre_compte.php">Votre Compte</a></li>
+ 			<li class="nav-item"><a class="nav-link" href="#">Admin</a></li>
+ 			<li class="nav-item"><a class="nav-link" href="panier.php">Panier</a></li>
+ 		</ul>
+ 		</div>
+	</nav>
+	<header class="page-header header container-fluid">
