67853eca021dfd9e211b57ee138a7ef4c78163e8
Breton Remi <remib37@gmail.com>
2020-04-18 15:45:41 +0200

    ajout nom du vendeur sur l'annonce de l'objet

--- acheter.php
+++ acheter.php
@@ -9,9 +9,12 @@ require 'db.php';
 			<tr><th>Produits : </th> <th>Description : </th><th>Prix : </th></tr>
 			<a href="categories.php" style="margin-left: 50px;">Afficher par catégories</a>
 				<?php foreach ($req as $product):?>
+					<?php $reqname = $conn->query('SELECT pseudo FROM user WHERE id ='.$product['id_vendeur']); 
+					$pseudo = $reqname->fetch();?>
 					<tr>
 						<td><a href="produits/<?php echo $product['id'];?>.jpg"><img src="produits/<?php echo $product['id'];?>.jpg" style="width: 200px; height: 150px;"></a></td>
-					<td><p class="text-justify"><?php echo $product['description']; ?></p></td>
+					<td><p class="text-justify"><?php echo $product['description']; ?></p>
+						<p>Vendu par : <?php echo $pseudo['pseudo']; ?></p></td>
 					<td><?php echo $product['price'];?> €</td>
 					<?php if($product['Achat'] == 1){?>
 					<td><a class = "add" href="panier.php?id=<?php echo $product['id'];?>"><img src="img/panier.png" style="width: 30px;height: 30px;"></a></td><?php }else if($product['Achat'] == 2){?>
