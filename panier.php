65f9436f4d76d5e34cabd6ba59f8520f6ab5eeed
Lambert Antoine <antoine.lamb@yahoo.com>
2020-04-18 21:28:22 +0200

    travail dejeuner

--- panier.php
+++ panier.php
@@ -13,17 +13,22 @@ if(isset($_GET['del']))
 			<div class="Info" style="margin-left: 30px;padding-top: 30px;">
 				<h5 >Votre Panier :</h5>
 				<table class="table">
-
-				<?php if(isset($_SESSION['panier']) && $_SESSION['panier'] != null){$ids = array_keys($_SESSION['panier']); $products = $conn->query('SELECT * FROM objets WHERE id IN ('.implode(',', $ids).')');?><tr><th>Produits : </th> <th>Description : </th><th>Prix : </th></tr><?php foreach ($products as $product):?>
+				
+				<?php if(isset($_SESSION['panier']) && $_SESSION['panier'] != null){$ids = array_keys($_SESSION['panier']); $products = $conn->query('SELECT * FROM objets WHERE id IN ('.implode(',', $ids).')');?><tr><th>Produits : </th> <th>Description : </th><th>Prix : </th></tr><?php $prixtot = 0;
+					foreach ($products as $product):?>
 					<tr>
 					<td><a href="produits/<?php echo $product['id'];?>.jpg"><img src="produits/<?php echo $product['id'];?>.jpg" style="width: 200px; height: 150px;"></a></td>
 					<td><p><?php echo $product['name']; ?></p></td>
 					<td><?php echo $product['price'];?> €</td>
 					<td><a href="del.php?del=<?php echo $product['id'];?>">Supprimer du panier</a></td>
 					</tr>
-				<?php endforeach; } else{?><p>Votre panier est vide</p><?php } ?>
+				<?php $prixtot += $product['price'];
+					endforeach; 
+					echo "Prix total =".$prixtot;
+				} else{?><p>Votre panier est vide</p><?php } ?>
 				</table>
 				<a href="acheter.php">Continuer mes achats</a>
+				<a href="achatpanier.php">Acheter tout le panier</a>
 			</div>
 		</div>
 
